7b11877397f6196d97c4f7f461cf703e83690e8c
Cezar <cez.rata1@gmail.com>
2021-06-10 14:07:37 -0700

    did statistical testing

--- src/process.py
+++ src/process.py
@@ -3,9 +3,11 @@ import requests
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 
-KEY = '311bbfc7214b34671449b6d6f1e9f247'
+KEY = 'ecf9892d82720ed87aec38497493a73a'
+
 
 def open_json(ticker):
 	f = open(f'../data/engagement_data/{ticker}_data.txt',)
@@ -155,16 +157,16 @@ def corr_diff_day(eng, stock):
 	future_y = copy(y)
 	stock_x, stock_y = stock
 	
-	prev_x = rotate(prev_x, -1)
-	future_x = rotate(future_x, 1)
-	prev_y = rotate(prev_y, -1)
-	future_y = rotate(future_y, 1)
+	prev_x = rotate(prev_x, -10)
+	future_x = rotate(future_x, 10)
+	prev_y = rotate(prev_y, -10)
+	future_y = rotate(future_y, 10)
 	
 	prev_x, prev_y = trim(prev_x, prev_y, x)
 	future_x, future_y = trim(future_x, future_y, x, prev = False)
 
-	corr_prev =  coefficient(np.array(prev_y[4:]), np.array(stock_y))
-	corr_future = coefficient(np.array(future_y[4:]), np.array(stock_y))
+	corr_prev =  coefficient(np.array(prev_y), np.array(stock_y[5:]))
+	corr_future = coefficient(np.array(future_y), np.array(stock_y[5:]))
 	return corr_prev, corr_future
 
 
@@ -189,7 +191,7 @@ def display_daily_avg(data, ticker):
 	daily_x = [i[:10] for i in data][::-1][140:]
 	y = [j[0] + j[1]  for j in y.values][::-1]
 	y = [i/max(y) for i in y][140:]
-	
+
 	eng_daily_totals = {}
 	for i, time in enumerate(daily_x):
 		if time in eng_daily_totals:
@@ -217,25 +219,135 @@ def display_daily_avg(data, ticker):
 	plt.ylabel("Relative to maximum", fontsize=16)
 	plt.tight_layout()
 	plt.legend()
+
+	return percent_correct((list(eng_daily_avgs.keys()), list(eng_daily_avgs.values())), (list(stock_daily_avgs.keys()), list(stock_daily_avgs.values())), ticker)
 	#plt.show()
 	#plt.savefig(f'../images/daily_price_vs_eng/{ticker}_daily_eng_vs_price_per.png')
 
-	y = [i for i in list(eng_daily_avgs.values())[-len(list(stock_daily_avgs.values())):]]
-	cur_corr = coefficient(np.array(y), np.array(list(stock_daily_avgs.values())))
-	prev_corr, future_corr = corr_diff_day((list(eng_daily_avgs.keys()), list(eng_daily_avgs.values())), (list(stock_daily_avgs.keys()), list(stock_daily_avgs.values())))
-	return {'Stock': ticker, 'Same Day Correlation' : cur_corr, "Prev Day Correlation" : prev_corr, "Next Day Correlation" : future_corr}
-	
+	# y = [i for i in list(eng_daily_avgs.values())[-len(list(stock_daily_avgs.values())):]]
+	# cur_corr = coefficient(np.array(y), np.array(list(stock_daily_avgs.values())))
+	# prev_corr, future_corr = corr_diff_day((list(eng_daily_avgs.keys()), list(eng_daily_avgs.values())), (list(stock_daily_avgs.keys()), list(stock_daily_avgs.values())))
+	# return {'Stock': ticker, 'Same Day Correlation' : cur_corr, "Prev Day Correlation" : prev_corr, "Next Day Correlation" : future_corr}
+	# t_cur = calculate_t_test(np.array(y), np.array(list(stock_daily_avgs.values())), .05)
+	# return t_cur
+
 
 	'''
-
+	High Prev Day Correlation = Following the trend
+	High Next Day Correlation = Ahead of the trend
 	{'Stock': 'AMC', 'Same Day Correlation': 0.7836734791979197, 'Prev Day Correlation': -0.095002426197534, 'Next Day Correlation': 0.7522435819173727}
 	{'Stock': 'GME', 'Same Day Correlation': 0.7015547092161876, 'Prev Day Correlation': -0.15977934508996347, 'Next Day Correlation': 0.6473196702717451}
 	{'Stock': 'BB', 'Same Day Correlation': 0.7510425096787338, 'Prev Day Correlation': -0.0005532303271056227, 'Next Day Correlation': 0.7172539412716349}
 	{'Stock': 'SNDL', 'Same Day Correlation': 0.7200428404381856, 'Prev Day Correlation': -0.07856040160933374, 'Next Day Correlation': 0.6745332953594741}
 	{'Stock': 'TLRY', 'Same Day Correlation': 0.7148534722081824, 'Prev Day Correlation': -0.041808424581382016, 'Next Day Correlation': 0.6758162262791414}
 	{'Stock': 'NOK', 'Same Day Correlation': 0.5355591273765817, 'Prev Day Correlation': 0.2730968257985349, 'Next Day Correlation': 0.4012833566220041}
+
+
+	T-Test 
+	AMC (5.141029882342577e-05, True)
+	GME (2.1902759648971002e-18, True)
+	BB (4.196445396871837e-14, True)
+	SNDL (1.7236621209051704e-16, True)
+	TLRY (3.989543501641067e-23, True)
+	NOK (6.1759986290167304e-30, True)
 	'''
 
+
+def percent_growth(arr):
+	arr = [(arr[i] - arr[i-1])/arr[i-1] for i in range(1, len(arr))]
+	return arr
+	
+
+def calc_predictions(dict1, dict2, key):
+	dict1_items = dict1.items()
+	dict2_items = dict2.items()
+
+	for i, d in enumerate(dict1_items):
+		if i == len(dict2_items):
+			return 0
+		if d[0] == key:
+			idx_of_key = [i for i, e in enumerate(list(dict2_items)) if e[0] == key][0]
+			if list(dict1_items)[i][1] > 0 and list(dict2_items)[idx_of_key][1] > 0:
+				return 1
+			else:
+				return 0
+	return 0
+
+
+def number_of_up_days(arr):
+	up = 0
+	for i in arr:
+		if i > 0:
+			up += 1
+	return up
+
+
+def percent_correct(eng_data, stock_data, ticker):
+	eng_x, eng_y = eng_data
+	stock_x, stock_y = stock_data
+
+	new_eng_y = copy(eng_y)
+	new_stock_y = copy(stock_y)
+
+	percent_change_eng_y = percent_growth(new_eng_y)
+	percent_change_stock_y = percent_growth(stock_y)
+	
+	eng_dict = {}
+	for i in range(len(eng_x) - 1):
+		eng_dict[eng_x[i]] = percent_change_eng_y[i]
+
+	stock_dict = {}
+	for i in range(len(stock_x) - 1):
+		stock_dict[stock_x[i]] = percent_change_stock_y[i]
+
+	predictions = 0
+
+	for k in eng_dict:
+		if k in stock_dict:
+			predictions += calc_predictions(eng_dict, stock_dict, k) 
+
+	positive_days = number_of_up_days(percent_change_eng_y)
+
+	return f'{ticker} % chance of getting in early: {predictions/positive_days}'
+
+	'''
+	stock on target with engagement / total up days for that stock
+	AMC % chance of getting in early: 0.45454545454545453
+	GME % chance of getting in early: 0.38461538461538464
+	BB % chance of getting in early: 0.3076923076923077
+	SNDL % chance of getting in early: 0.2857142857142857
+	TLRY % chance of getting in early: 0.3333333333333333
+	NOK % chance of getting in early: 0.18181818181818182
+
+	stock follows engagement / total up days for that stock
+	AMC % chance of getting in early: 0.45454545454545453
+	GME % chance of getting in early: 0.38461538461538464
+	BB % chance of getting in early: 0.38461538461538464
+	SNDL % chance of getting in early: 0.35714285714285715
+	TLRY % chance of getting in early: 0.4166666666666667
+	NOK % chance of getting in early: 0.2727272727272727
+
+	stock on target with engagement / total up days for engagement
+	AMC % chance of getting in early: 0.45454545454545453
+	GME % chance of getting in early: 0.45454545454545453
+	BB % chance of getting in early: 0.36363636363636365
+	SNDL % chance of getting in early: 0.36363636363636365
+	TLRY % chance of getting in early: 0.36363636363636365
+	NOK % chance of getting in early: 0.2
+
+	stock follows engagement / total up days for engagement
+	AMC % chance of getting in early: 0.45454545454545453
+	GME % chance of getting in early: 0.45454545454545453
+	BB % chance of getting in early: 0.45454545454545453
+	SNDL % chance of getting in early: 0.45454545454545453
+	TLRY % chance of getting in early: 0.45454545454545453
+	NOK % chance of getting in early: 0.3
+	'''
+
+
+def calculate_t_test(x, y, a):
+	_, p_value = stats.ttest_ind(x, y)
+	return p_value, p_value < a
 
 
 def display_eng():
